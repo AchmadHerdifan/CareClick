b0d19bdd246ce6c18270e8ede202bc633e0674e8
AchmadHerdifan <ifanpratama1711@gmail.com>
2026-07-28 16:12:01 +0700

    update env

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          50:  '#eff6ff',
+          50: '#eff6ff',
           100: '#dbeafe',
           500: '#3b82f6',
           600: '#2563eb',
